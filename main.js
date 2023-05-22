06fed539bc1f665735b90184812df18b30e8a7c3
Powwow84 <pao.saelee84@gmail.com>
2023-05-22 14:02:05 -0700

    got the randomly gnerated maze to appear

--- main.js
+++ main.js
@@ -1,163 +1,203 @@
 import * as THREE from 'three';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
 import * as CANNON from 'cannon-es';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui'
+import maze from './mapArray';
+
 
 // --------------Three Js setup ----------------
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+// Set up renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true; // Enable shadow mapping
 document.body.appendChild(renderer.domElement);
 
-// Create a plane as the ground
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 20, 10);
+// Create a scene
+const scene = new THREE.Scene();
+
+// Add OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// -------------------------------------------------------------
+
+// --------------Cannon ES Set up ----------------------------
+
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
+
+// const concreteMaterial = new CANNON.Material('concrete')
+// const plasticMaterial = new CANNON.Material('plastic')
+
+const defaultMaterial = new CANNON.Material('default')
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7
+  }
+)
+world.addContactMaterial(defaultContactMaterial)
+world.defaultContactMaterial = defaultContactMaterial
+
+// const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+//   concreteMaterial,
+//   plasticMaterial,
+//   {
+//     friction: 0.1,
+//     restitution: 1
+//   }
+// )
+// world.addContactMaterial(concretePlasticContactMaterial)
+
+// Create a plane
 const planeGeometry = new THREE.PlaneGeometry(10, 10);
-const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide });
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = Math.PI / 2;
-scene.add(plane);
+const planeMaterial = new THREE.MeshPhongMaterial({ color: 0xeeeeee });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.receiveShadow = true; // Enable shadow casting on the plane
+scene.add(planeMesh);
+planeMesh.rotation.x = -0.5* Math.PI
 
-// Create a box as a wall
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const box = new THREE.Mesh(boxGeometry, boxMaterial);
-box.position.y = 0.5;
-scene.add(box);
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body()
+// floorBody.material = defaultContactMaterial
+floorBody.mass = 0
+floorBody.addShape(floorShape)
+floorBody.quaternion.setFromAxisAngle(
+  new CANNON.Vec3(-1, 0, 0),
+  Math.PI * .5
+)
+world.addBody(floorBody)
 
-// Set up physics world
-const world = new CANNON.World();
-world.gravity.set(0, -9.82, 0);
+// Create a sphere
+// const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+// const sphereMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+// const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+// sphereMesh.position.set(0, 1, 0);
+// sphereMesh.castShadow = true; // Enable shadow casting from the sphere
+// scene.add(sphereMesh);
 
-// Add ground plane as a static body
-const groundShape = new CANNON.Plane();
-const groundBody = new CANNON.Body({ mass: 0 });
-groundBody.addShape(groundShape);
-world.addBody(groundBody);
+// const sphereShape = new CANNON.Sphere(1)
+// const sphereBody = new CANNON.Body({
+//   mass: 1,
+//   position: new CANNON.Vec3(0, 10, 0),
+//   shape: sphereShape,
+//   // material: defaultContactMaterial
+// })
+// sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0))
+// world.addBody(sphereBody)
 
-// Add box as a static body
-const boxShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
-const boxBody = new CANNON.Body({ mass: 0 });
-boxBody.addShape(boxShape);
-boxBody.position.y = 0.5;
-world.addBody(boxBody);
 
-// Add camera as a dynamic body
-const cameraShape = new CANNON.Sphere(0.25);
-const cameraBody = new CANNON.Body({ mass: 1 });
-cameraBody.addShape(cameraShape);
-cameraBody.position.y = 1;
-world.addBody(cameraBody);
 
-// Enable keyboard controls for camera movement
-const keyboard = {};
-const moveSpeed = 0.1;
+// Create lighting
+const ambientLight = new THREE.AmbientLight(0x404040); // Ambient light
+scene.add(ambientLight);
 
-function handleKeyDown(event) {
-  keyboard[event.code] = true;
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Directional light
+directionalLight.position.set(0, 10, 0);
+directionalLight.castShadow = true; // Enable shadow casting from the light
+scene.add(directionalLight);
+
+
+// ----------------Utilities-----------
+
+const objectsToUpdate = []
+
+
+const createSphere = (radius, position) => {
+  // threejs mesh
+  const mesh = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(radius, 20, 20),
+    new THREE.MeshStandardMaterial({
+      metalness: 0.3,
+      roughness: 0.4,
+      // envMap: environmentMapTexture
+    })
+  )
+  mesh.castShadow = true
+  mesh.position.copy(position)
+  scene.add(mesh)
+
+  //  cannon shapes
+  const shape = new CANNON.Sphere(radius)
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: shape,
+    material: defaultMaterial
+  })
+  body.position.copy(position)
+  world.addBody(body)
+
+  // save in objects to update
+  objectsToUpdate.push({
+    mesh: mesh,
+    body: body
+  })
 }
 
-function handleKeyUp(event) {
-  keyboard[event.code] = false;
-}
+// createSphere(0.5, {x: 0, y: 3, z: 0})
 
-document.addEventListener('keydown', handleKeyDown);
-document.addEventListener('keyup', handleKeyUp);
+function createMaze() {
+  // Array representing the maze layout
 
-// Pointer lock controls for camera look
-const controls = {
-  pitch: 0,
-  yaw: 0
-};
+  // Dimensions of the maze
+  const numRows = maze.length;
+  const numCols = maze[0].length;
 
-function handleMouseMove(event) {
-  controls.yaw -= event.movementX * 0.002;
-  controls.pitch -= event.movementY * 0.002;
-  controls.pitch = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, controls.pitch));
-}
+  // Size of each cell in the maze
+  const cellSize = 2;
 
-function lockPointer() {
-  pointerControls.lock();
-}
+  // Starting position of the maze
+  const startX = -((numCols - 1) * cellSize) / 2;
+  const startZ = -((numRows - 1) * cellSize) / 2;
 
-document.addEventListener('mousemove', handleMouseMove);
-document.addEventListener('click', lockPointer);
-
-// Add pointer lock controls
-const pointerControls = new PointerLockControls(camera, document.body);
-scene.add(pointerControls.getObject());
-
-// Animation loop
-function animate() {
-  requestAnimationFrame(animate);
-
-  // Move camera based on keyboard input
-  const force = new CANNON.Vec3();
-  if (keyboard['ArrowUp'] || keyboard['KeyW']) {
-    const forward = new THREE.Vector3();
-    pointerControls.getDirection(forward);
-    forward.y = 0;
-    forward.normalize();
-    force.z = -moveSpeed;
-  }
-  if (keyboard['ArrowDown'] || keyboard['KeyS']) {
-    const backward = new THREE.Vector3();
-    pointerControls.getDirection(backward);
-    backward.y = 0;
-    backward.normalize();
-    force.z = moveSpeed;
-  }
-  if (keyboard['ArrowLeft'] || keyboard['KeyA']) {
-    const left = new THREE.Vector3();
-    pointerControls.getDirection(left);
-    left.cross(camera.up);
-    left.y = 0;
-    left.normalize();
-    force.x = -moveSpeed;
-  }
-  if (keyboard['ArrowRight'] || keyboard['KeyD']) {
-    const right = new THREE.Vector3();
-    pointerControls.getDirection(right);
-    right.cross(camera.up);
-    right.y = 0;
-    right.normalize();
-    force.x = moveSpeed;
-  }
-  // cameraBody.velocity.copy(force);
-
-  // Prevent camera from falling through the plane
-  const minY = 0.5;
-  if (cameraBody.position.y < minY) {
-    cameraBody.position.y = minY;
-    // cameraBody.velocity.y = 0;
-  }
-
-  // Check collision between camera and box
-  const result = new CANNON.RaycastResult();
-  const from = camera.position.clone();
-  const to = camera.position.clone().add(camera.getWorldDirection(new THREE.Vector3()).multiplyScalar(0.1));
-
-  world.raycastAny(from, to, { skipBackfaces: true }, result);
-
-  if (result.hasHit) {
-    const distance = result.distance;
-    if (distance < 0.1) {
-      const separation = 0.1 - distance;
-      const correction = camera.getWorldDirection(new THREE.Vector3()).multiplyScalar(separation);
-      // cameraBody.velocity.sub(correction);
+  // Create spheres based on maze layout
+  for (let row = 0; row < numRows; row++) {
+    for (let col = 0; col < numCols; col++) {
+      if (maze[row][col] === 0) {
+        const positionX = startX + col * cellSize;
+        const positionZ = startZ + row * cellSize;
+        createSphere(0.5, { x: positionX, y: 0, z: positionZ });
+      }
     }
   }
+}
 
-  // Apply rotation to camera
-  camera.rotation.order = 'YXZ';
-  camera.rotation.y = controls.yaw;
-  camera.rotation.x = controls.pitch;
+createMaze()
 
-  // Update physics simulation
-  world.step(1 / 60);
+const clock = new THREE.Clock()
+let oldElapsedTIme = 0
 
-  // Update camera position
-  camera.position.copy(cameraBody.position);
+// Render loop
+function animate() {
 
-  // Render the scene
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - oldElapsedTIme
+  oldElapsedTIme = elapsedTime
+
+  // sphereBody.applyForce(new CANNON.Vec3(-.05, 0, 0), sphereBody.position)
+
+  // update physics world
+  world.step(1/60, deltaTime, 3 )
+
+  for(const object of objectsToUpdate) {
+    object.mesh.position.copy(object.body.position)
+  }
+  // sphereMesh.position.copy(sphereBody.position)
+
+
+
+  controls.update()
+  requestAnimationFrame(animate);
+
+
+
+  
   renderer.render(scene, camera);
 }
 
