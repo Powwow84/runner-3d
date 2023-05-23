81af628de19725b91b12c954aa8bd41ef2c17d46
Powwow84 <pao.saelee84@gmail.com>
2023-05-23 12:57:18 -0700

    got text rendering in environment

--- main.js
+++ main.js
@@ -2,15 +2,183 @@ import * as THREE from 'three';
 import {PointerLockControls} from 'three/examples/jsm/controls/PointerLockControls';
 import * as CANNON from 'cannon-es';
 import maze from './mapArray';
-import CannonDebugger from 'cannon-es-debugger';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
-// --------------Three Js setup ----------------
-// Set up renderer
+
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.domElement.requestPointerLock = renderer.domElement.requestPointerLock || renderer.domElement.mozRequestPointerLock;
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true; // Enable shadow mapping
 document.body.appendChild(renderer.domElement);
+
+const startScreen = () => {
+
+  // Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 30, 40);
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+// Create a scene
+const scene = new THREE.Scene();
+
+// Setup font ---------------------
+const loader = new FontLoader();
+
+loader.load( 'https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function ( font ){
+
+	const fontgeometry = new TextGeometry( 'Hello three.js!', {
+		font: font,
+		size: 2,
+		height: 0,
+		curveSegments: 12,
+		bevelEnabled: true,
+		bevelThickness: 0,
+		bevelSize: 1,
+		bevelOffset: 0,
+		bevelSegments: 5
+	} );
+  const fontMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  
+  const textMesh = new THREE.Mesh(fontgeometry, fontMaterial);
+  scene.add(textMesh);
+  textMesh.position.set(0, 5, 1);
+} );
+
+
+
+// Add OrbitControls
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.update()
+
+
+
+// -----------------Creating the cube texture for the world environment
+const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+scene.background = cubeTextureLoader.load([
+  'public/imgs/Untitled design/1.jpg',
+  'public/imgs/Untitled design/3.jpg',
+  'public/imgs/Untitled design/2.jpg',
+  'public/imgs/Untitled design/4.jpg',
+  'public/imgs/Untitled design/5.jpg',
+  'public/imgs/Untitled design/6.jpg',
+  
+])
+
+// Create a plane
+const planeGeometry = new THREE.PlaneGeometry(130, 100);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  map: textureLoader.load('public/imgs/Untitled design/4.jpg') });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.receiveShadow = true; // Enable shadow casting on the plane
+scene.add(planeMesh);
+planeMesh.rotation.x = -0.5 * Math.PI
+
+
+// Create lighting
+const ambientLight = new THREE.AmbientLight(0x404040); // Ambient light
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Directional light
+directionalLight.position.set(0, 10, 0);
+directionalLight.castShadow = true; // Enable shadow casting from the light
+scene.add(directionalLight);
+
+
+// ----------------Utilities-----------
+
+const objectsToUpdate = []
+
+// create box's
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+  map: textureLoader.load('public/imgs/Untitled design/AdobeStock_242318067.jpeg') 
+})
+
+
+const createBox = (width, height, depth, position) => {
+  // threejs mesh
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  mesh.scale.set(width, height, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // cannon shapes
+  // const shape = new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth / 2));
+  // const body = new CANNON.Body({
+  //   mass: 10, //0.000001,
+  //   type: 2,
+  //   position: new CANNON.Vec3(0, 0, 0),
+  //   shape: shape,
+  //   material: new CANNON.Material() // Using CANNON.Material directly
+  // });
+  // body.position.copy(position);
+  // // body.addEventListener('collide', playSfx) example to use when we are doing the win condition
+  // world.addBody(body);
+
+  // save in objects to update
+  objectsToUpdate.push({
+    mesh: mesh,
+    // body: body
+  });
+};
+
+function createMaze() {
+  // Dimensions of the maze
+  const numRows = maze.length;
+  const numCols = maze[0].length;
+
+  // Size of each cell in the maze
+  const cellSize = 1;
+
+  // Calculate the adjusted spacing between each box
+  const adjustedSpacing = (cellSize - 0.5) / 2;
+
+  // Starting position of the maze
+  const startX = -((numCols - 1) * cellSize) / 2 + adjustedSpacing;
+  const startZ = -((numRows - 1) * cellSize) / 2 + adjustedSpacing;
+
+  // Create boxes based on maze layout
+  for (let row = 0; row < numRows; row++) {
+    for (let col = 0; col < numCols; col++) {
+      if (maze[row][col] === 1) { // Flip condition from 0 to 1
+        const positionX = startX + col * cellSize;
+        const positionZ = startZ + row * cellSize;
+        createBox(1, 3, 1, { x: positionX, y: 1, z: positionZ });
+      }
+    }
+  }
+}
+
+createMaze();
+
+
+
+// Render loop
+function animate() {
+
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  
+}
+animate();
+
+window.addEventListener('resize', function(){
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+})
+
+}
+
+
+const gameInit = () => {
+// --------------Three Js setup ----------------
+// Set up renderer
 
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -24,9 +192,9 @@ const scene = new THREE.Scene();
 
 // ----------------------------Files---------------------------------
 
-// const bgMusic = new Audio('public/music/tunetank.com_5196_secrets-of-the-house-on-the-hill_by_rage-sound.mp3')
+const bgMusic = new Audio('public/music/tunetank.com_5196_secrets-of-the-house-on-the-hill_by_rage-sound.mp3')
 // const sfx = new Audio('public/music/661499__het_hckm_ds_huis__mortality-boring-death-dying-clock-tick-tock-klok-tik-tak-incl-20-hertz-sometimes-02-01.mp3')
-// // bgMusic.play()
+bgMusic.play()
 
 // const playSfx = () =>
 // {
@@ -171,7 +339,7 @@ function createMaze() {
       if (maze[row][col] === 1) { // Flip condition from 0 to 1
         const positionX = startX + col * cellSize;
         const positionZ = startZ + row * cellSize;
-        createBox(1, 6, 1, { x: positionX, y: 1, z: positionZ });
+        createBox(1, 3, 1, { x: positionX, y: 1, z: positionZ });
       }
     }
   }
@@ -300,3 +468,10 @@ window.addEventListener('resize', function(){
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
+
+}
+
+
+// gameInit()
+
+startScreen()
